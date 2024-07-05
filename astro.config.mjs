2dac68ad77c521e77229777822a7ead7f2781496
xtwgok3 <xtwgok@gmail.com>
2024-07-05 15:41:39 -0300

    optimizacion 1 astro conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,63 @@
-import cloudflare from "@astrojs/cloudflare"
-import tailwind from "@astrojs/tailwind"
-import { defineConfig } from "astro/config"
+import cloudflare from "@astrojs/cloudflare";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
+import { VitePWA } from "vite-plugin-pwa";
+import viteCompression from "vite-plugin-compression";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
-	adapter: cloudflare(),
-	output: "hybrid",
-	vite: {
-		build: {
-			cssMinify: "lightningcss",
-			// @ts-ignore
-			static: "/dist/_astro/",
-		},
-		ssr: {
-			noExternal: ["path-to-regexp"],
-		},
-	},
-})
+  integrations: [tailwind(), sitemap()],
+  adapter: cloudflare(),
+  output: "hybrid",
+  vite: {
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
+        manifest: {
+          name: 'Astro App',
+          short_name: 'AstroApp',
+          description: 'An awesome Astro application',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
+      }),
+      viteCompression({
+        algorithm: 'brotliCompress',
+        ext: '.br',
+      }),
+      viteCompression({
+        algorithm: 'gzip',
+        ext: '.gz',
+      }),
+    ],
+    build: {
+      cssMinify: "lightningcss",
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
+          }
+        }
+      },
+      staticDir: "/dist/_astro/",  // Corrección de la propiedad
+    },
+    ssr: {
+      noExternal: ["path-to-regexp"],
+    },
+  },
+});// código uno test optimizacion
